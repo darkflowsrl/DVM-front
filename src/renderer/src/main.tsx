26f6a9000b5c0bf7d6ad0fe79e382fa84f43f6a4
Fede Rodriguez <tec.federodriguez@gmail.com>
2025-06-27 11:38:54 -0300

    Remove strict mode

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './ui/global.css'
 import './ui/keyboard.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
 
 // Remove Preload scripts loading
