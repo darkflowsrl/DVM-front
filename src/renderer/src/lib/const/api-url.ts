c8cf02662c0064ff08b1062159e48c38ae6992cd
Fede Rodriguez <fede4rodriguez@outlook.com>
2025-07-14 14:41:59 -0300

    Fix url

--- src/renderer/src/lib/const/api-url.ts
+++ src/renderer/src/lib/const/api-url.ts
@@ -2,6 +2,6 @@ import { IS_DEVELOPMENT } from './env'
 
 export const API_URL: string = IS_DEVELOPMENT
   ? 'http://192.168.1.187:8080'
-  : `http://${window.location.hostname}:8080`
+  : `http://127.0.0.1:8080`
 
 console.log('API_URL', API_URL)
